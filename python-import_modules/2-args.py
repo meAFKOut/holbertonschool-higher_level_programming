b2d06e1cc47fc6466326a9973c1b2c6511d323d2
meAFKOut <meafkoutback@gmail.com>
2025-01-10 12:40:45 +0400

    84748

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,13 +1,18 @@
-#!/usr/bin/python3
+#!/usr/bin/python3  
+import sys  
 
-from sys import argv
-if __name__ == "__main__":
-    argc = len(argv) - 1
-    if argc == 0:
-        print("0 arguments.")
-    elif argc == 1:
-        print("1 argument:\n1: {}".format(argv[1]))
-    else:
-        print("{} arguments:".format(argc))
-        for i in range(1, argc + 1):
-            print("{}: {}".format(i, argv[i]))
+def main():  
+    arguments = sys.argv[1:]  
+    num_arguments = len(arguments)  
+
+    if num_arguments == 0:  
+        print("0 arguments.")  
+    else:  
+        argument_label = "argument" if num_arguments == 1 else "arguments"  
+        print(f"{num_arguments} {argument_label}:")  
+        
+        for i in range(num_arguments):  
+            print(f"{i + 1}: {arguments[i]}")  
+
+if __name__ == '__main__':  
+    main() 
