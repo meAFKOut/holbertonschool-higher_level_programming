5ef79ade08ab7bfea492e80aa65de3421b70be4f
meAFKOut <meafkoutback@gmail.com>
2025-02-03 06:40:13 +0000

    76484

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -6,11 +6,11 @@ def lookup(obj):
     """
     Return a list of available attributes and methods of an object.
 
-    Ar
+    Args:
         obj: The object whose attributes and methods are to be returned.
 
     Returns:
         list: A list of strings representing the attributes and methods
-        of th object.
-    ""
+        of the object.
+    """
     return dir(obj)
