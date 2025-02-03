0619514371a8112e0dbd1737658a55f7c4dd8d02
Mr.AFK <meafkoutback@gmail.com>
2025-02-03 10:01:18 +0400

    Update 7-add_tuple.py

--- python-data_structures/7-add_tuple.py
+++ python-data_structures/7-add_tuple.py
@@ -5,7 +5,7 @@ def add_tuple(tuple_a=(), tuple_b=()):
     for i in range(2):
         aList.append(0)
         bList.append(0)
-    sum_0 = aList[0] + bList[0]
+    sum_0 = aList[0] + bLt[0]
     sum_1 = aList[1] + bList[1]
     tuple_c = (sum_0, sum_1)
     return tuple_c
