a4b0b55dbc9b9148563ce205f808b49a89da320f
meAFKOut <meafkoutback@gmail.com>
2025-02-03 06:51:45 +0000

    8494

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-""This module contains the MyList class that inherits from the list class."""
+"""This module contains the MyList class that inherits from the list class."""
 
 
 class MyList(list):
@@ -10,7 +10,8 @@ class MyList(list):
     -------
     print_sorted():
         Prints the list in ascending order.
-    ""
+    """
     def print_sorted(self):
-        """Returns a new list in asc order.""
+        """Returns a new list in ascending order."""
         print(sorted(self))
+        return sorted(self)
