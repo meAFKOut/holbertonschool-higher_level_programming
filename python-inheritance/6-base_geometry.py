dcb8ec8006b36697d9b67e6593d2bbd0c5d3c599
meAFKOut <meafkoutback@gmail.com>
2025-02-03 08:36:28 +0000

    876383

--- python-inheritance/6-base_geometry.py
+++ python-inheritance/6-base_geometry.py
@@ -1,12 +1,22 @@
 #!/usr/bin/python3
+"""This class contains class named BaseGeometry"""
 
-""This clasass named BaseGeometry""
 
-	class BaseGeometry
-	  ""
-		BaseGeometry class 
+class BaseGeometry:
+    """
+       BaseGeometry class
 
-		Method
-		------
-			area():
-	  ""
+       Method
+       ------
+            area():
+    """
+    def area(self):
+        """
+        Area method
+
+        Raise
+        -----
+            Exception:
+                area() is not implemented
+        """
+        raise Exception("area() is not implemented")
