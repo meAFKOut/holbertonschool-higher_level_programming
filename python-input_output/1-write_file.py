a21527301239d0d71cc4eae46d7137f5120d5b07
meAFKOut <meafkoutback@gmail.com>
2025-02-10 07:16:25 +0000

    879

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -1,6 +1,12 @@
 #!/usr/bin/python3
 
+def read_file(filename=""):
+    """
+    Reads the content of a file and prints it.
 
-def read_file(filename = "") :
-    with open(filename, "r", encoding = "utf-8") as file:
-        print(file.read() , end="") 
+    Args:
+    filename (str): The name of the file to read. Default is an empty string.
+    """
+    with open(filename, "r", encoding="utf-8") as file:
+        print(file.read(), end="")
+
