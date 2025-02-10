55dae9733dd04cb05c8ea4c014a6b1025d5e59ea
meAFKOut <meafkoutback@gmail.com>
2025-02-10 07:22:36 +0000

    93903

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -1,16 +1,16 @@
 #!/usr/bin/python3
 
-def read_file(filename = "" , text = ""):
-"""
-Writes a string to a text file (UTF-8) and returns the number of characters written.
+def write_file(filename="", text=""):
+    """
+    Writes a string to a text file (UTF-8) and returns the number of characters written.
 
-Args:
-filename (str): The name of the file to write to. Default is an empty string.
-text (str): The string to write to the file. Default is an empty string.
+    Args:
+    filename (str): The name of the file to write to. Default is an empty string.
+    text (str): The string to write to the file. Default is an empty string.
 
-Returns:
-int: The number of characters written to the file.
-"""
-
-	return file.write(text)
+    Returns:
+    int: The number of characters written to the file.
+    """
+    with open(filename, "w", encoding="utf-8") as file:
+        return file.write(text)
 
