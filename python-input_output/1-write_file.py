ce6469d41567bd7e4703c4ee9f2370fb62abcf50
meAFKOut <meafkoutback@gmail.com>
2025-02-10 07:21:13 +0000

    940

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -1,12 +1,16 @@
 #!/usr/bin/python3
 
-def read_file(filename=""):
-    """
-    Reads the content of a file and prints it.
+def read_file(filename = "" , text = ""):
+"""
+Writes a string to a text file (UTF-8) and returns the number of characters written.
 
-    Args:
-    filename (str): The name of the file to read. Default is an empty string.
-    """
-    with open(filename, "r", encoding="utf-8") as file:
-        print(file.read(), end="")
+Args:
+filename (str): The name of the file to write to. Default is an empty string.
+text (str): The string to write to the file. Default is an empty string.
+
+Returns:
+int: The number of characters written to the file.
+"""
+
+	return file.write(text)
 
