56da4befb00c8b9f15e293c26779f95d6f9dddd6
Mr.AFK <meafkoutback@gmail.com>
2025-04-09 06:21:32 +0000

    34567

--- javascript-dom_manipulation/3-script.js
+++ javascript-dom_manipulation/3-script.js
@@ -1,4 +1,11 @@
 const $ = window.$;
-$('#red_header').click(function () {
-  $('header').addClass('red');
+$('#toggle_header').click(function () {
+  let header = $('header');
+  if (header.hasClass('red')) {
+    header.removeClass('red');
+    header.addClass('green');
+  } else {
+    header.removeClass('green');
+    header.addClass('red');
+  }
 });
